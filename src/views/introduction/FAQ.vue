ecf0fbdffbe38a07e01c1127ab27eb439260519c
Graham Sutton <grahamsutton2@gmail.com>
2019-05-02 17:48:30 -0400

    Added Law of Macaw page and updated FAQ with more questions.

--- src/views/introduction/FAQ.vue
+++ src/views/introduction/FAQ.vue
@@ -8,7 +8,10 @@
             <h5>Contents</h5>
             <b-list-group-item @click="scrollIntoView" href="#item-1">What is a Design System?</b-list-group-item>
             <b-list-group-item @click="scrollIntoView" href="#item-2">How do I use Macaw?</b-list-group-item>
-            <b-list-group-item @click="scrollIntoView" href="#item-3">What if I'm not implementing a component using Vue?</b-list-group-item>
+            <b-list-group-item @click="scrollIntoView" href="#item-3">When should I use Vue?</b-list-group-item>
+            <b-list-group-item @click="scrollIntoView" href="#item-4">Can I use Vue with ExtJS and jQuery?</b-list-group-item>
+            <b-list-group-item @click="scrollIntoView" href="#item-5">What if I'm not implementing a component using Vue?</b-list-group-item>
+            <b-list-group-item @click="scrollIntoView" href="#item-6">Why Vue? Why not Angular or React?</b-list-group-item>
           </b-list-group>
         </div>
       </div>
@@ -47,13 +50,40 @@
 
 			<p><strong>Adding a notification to an existing page?</strong> Check out the notification page to grab the markup needed.</p>
 
-			<h3 id="item-3">What if I'm not implementing a component using Vue?</h3>
+      <h3 id="item-3">When should I use Vue?</h3>
+      <p><strong>Short answer:</strong> Only use Vue for <strong><u>brand new code</u></strong>.</p>
+
+      <p><strong>Long(er) answer:</strong> Macaw brings in a lot of new styles that fit our more current and modern theme. In addition, a lot of code in the main SproutLoud application uses older libraries such as ExtJS and jQuery. These older styles and JavaScript libraries can create complications when working with Vue, so we recommend that using Vue should be reserved only for brand new or completely refactored code.</p>
+
+      <h3 id="item-4">Can I use Vue with ExtJS and jQuery?</h3>
+      <p><strong>Short answer:</strong>  Try to avoid mixing Vue with ExtJS at all costs. Don't use jQuery in Vue at all (it is not necessary).</p>
+
+      <p><strong>Long(er) answer:</strong> Although ExtJS and Vue can work together in many cases, using ExtJS with Vue can create unnecessary dependencies. Vue components are intended to be reusable throughout other parts of the application. ExtJS components often are very specific to one part of the application once they are defined. In addition, ExtJS provides it's only styling that does not match the our newer and more modern theme.</p>
+
+			<h3 id="item-5">What if I'm not implementing a component using Vue?</h3>
 			<p><strong>Short answer:</strong> No problem. There are 2 tabs for the code, Vue and HTML. Simply grab the HTML code to use within the app. You'll also need to add the class "macaw" to a parent div to grab the styles.</p>
 
 			<p><strong>Long(er) answer:</strong> Macaw will be implemented incrementally to ensure it's success and the success of the teams. In a perfect world, we'd be able to stop time and implement Macaw everywhere on our app, but unfortunately that's not an option yet. So, all of our styles for components are available to use within the app, as long as the parent div contains "macaw". This allows us to have the Macaw styles ready on-demand, without interferring with exisiting patterns.</p>
+
+      <h3 id="item-6">Why Vue? Why not Angular or React?</h3>
+      <p>The reason we chose Vue is because it is incrementally adoptable and has a much simpler learning curve. Although Angular and React are certainly great frameworks, they both have their limitations for SproutLoud: </p>
+      <ul>
+        <li>Angular cannot be used in the app sparingly (either everything is Angular or nothing is Angular).</li>
+        <li>React <em>can</em> be adopted incrementally, but does not offer the flexibility of <a href="https://vuejs.org/v2/guide/single-file-components.html" target="_blank">Single File Components <icon name="external-link" size="xs"></icon></a> like Vue, has a slightly more challenging learning curve, and would make directory organization more complicated. It would also require us to download more dependencies to support JSX.</li>
+      </ul>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import Icon from '@/elements/Icon.vue'
+
+export default {
+  components: {
+    Icon
+  }
+}
+</script>
---
+++ src/views/standards/LawOfMacaw.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="row">
+      <!-- main container block -->
+      <div class="col order-md-2 col-lg-3 offset-lg-1 px-0 ml-0"></div>
+      <!-- / inner navigation -->
+
+      <!-- main container block -->
+      <div class="col-12 col-md-8 order-md-1 col-lg-8 offset-lg-1 py-5 pr-5" ref="content">
+        <!-- Here goes the content of the page -->
+        <div class="row">
+
+          <!-- Title Container-->
+          <div class="col-12 titles-ct mt-0">
+            <h1>Law of Macaw</h1>
+            <h2>Every time you break a law, God will smite a kitten.</h2>
+          </div>
+          <!-- / Title Container-->
+
+          <div class="col-12">
+            <hr>
+          </div>
+
+          <!-- Page description text -->
+          <div class="col-12">
+            <ol>
+              <li>
+                <h3>Thou shall <em>not</em> use jQuery inside a Vue component.</h3>
+                <p>jQuery selectors should never be used inside a Vue component. Instead, <a href="https://vuejs.org/v2/api/#ref"><code>ref</code> <icon name="external-link" size="xs"></icon></a> and <a href="https://vuejs.org/v2/guide/events.html#ad">event handlers <icon name="external-link" size="xs"></icon></a> must be used. Failure to obey is punishable by public humiliation.</p>
+              </li>
+              <li>
+                <h3>Thou shall <em>not</em> use ExtJS inside a Vue component.</h3>
+                <p>ExtJS should never be used inside a Vue component. You must use one or the other, but never both. Failure to obey is punishable by public humiliation.</p>
+              </li>
+              <li>
+                <h3>Thou shall use Vue when creating brand new code.</h3>
+                <p>Vue and ES6 "new" JavaScript should be used when creating brand new or completely refactored code. Failure to obey is punishable by public humiliation.</p>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '@/elements/Icon.vue'
+
+export default {
+  components: {
+    Icon
+  },
+  methods: {
+    scrollIntoView (evt) {
+      evt.preventDefault()
+
+      const href = evt.target.getAttribute('href')
+      const el = href ? document.querySelector(href) : null
+
+      if (el) {
+        this.$refs.content.scrollTop = el.offsetTop
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#nav-scroller {
+  position: relative; 
+  height: 100vh; 
+  overflow-y: scroll;
+}
+
+.nav {
+  .nav-item {
+    .nav-link {
+      padding: .2rem 1rem;
+    }
+  }
+}
+</style>
